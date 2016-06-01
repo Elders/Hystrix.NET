36f59d84dc6e3a6ad17ef2e59f7147d28161b06d
ethno2405 <k.tenekedzhiev@gmail.com>
2016-06-01 18:10:09 +0300

    AtlasPublisher

--- src/Hystrix.Example/GetCurrentTimeCommand.cs
+++ src/Hystrix.Example/GetCurrentTimeCommand.cs
@@ -1,10 +1,8 @@
 ﻿namespace Hystrix.Example
 {
-    using System;
-    using System.Net;
-    using System.Xml.Linq;
     using Netflix.Hystrix;
-
+    using System;
+    using System.Threading;
     public class GetCurrentTimeCommand : HystrixCommand<long>
     {
         private static long currentTimeCache;
@@ -22,13 +20,24 @@
 
         protected override long Run()
         {
-            using (WebClient wc = new WebClient())
-            {
-                string content = wc.DownloadString("http://tycho.usno.navy.mil/cgi-bin/time.pl");
-                XDocument document = XDocument.Parse(content);
-                currentTimeCache = long.Parse(document.Element("usno").Element("t").Value);
-                return currentTimeCache;
-            }
+            var random = new Random().Next(1000);
+
+            if (random % 3 == 0)
+                throw new Exception();
+            if (random % 226 == 0)
+                Thread.Sleep(random * 20);
+            else
+                Thread.Sleep(random);
+
+            return random;
+
+            //using (WebClient wc = new WebClient())
+            //{
+            //    string content = wc.DownloadString("http://tycho.usno.navy.mil/cgi-bin/time.pl");
+            //    XDocument document = XDocument.Parse(content);
+            //    currentTimeCache = long.Parse(document.Element("usno").Element("t").Value);
+            //    return currentTimeCache;
+            //}
         }
 
         protected override long GetFallback()
